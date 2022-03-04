3a002b0b34ae2c555d60402fd50f18426c9433b2
Tolulope <tolulope@pop-os.localdomain>
2022-03-03 20:43:51 -0800

    cleaning up App.js

--- App.js
+++ App.js
@@ -2,12 +2,6 @@ import 'react-native-gesture-handler';
 
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Ionicons } from '@expo/vector-icons';
-
 import { MainStackNavigator } from './navigation/StackNavigator';  
 
 import AppLoading from 'expo-app-loading';
@@ -32,81 +26,6 @@ import {  Montserrat_100Thin,
   Montserrat_900Black_Italic  } from '@expo-google-fonts/montserrat';
 
 
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import LibraryPage from './pages/LibraryPage';
-import CompletedPage from './pages/CompletedPage';
-import LikedPage from './pages/LikedPage';
-import InProgressPage from './pages/InProgressPage';
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-const TopTab = createMaterialTopTabNavigator();
-
-function TopTabs() {
-  return (
-    <TopTab.Navigator 
-      screenOptions={{
-        tabBarActiveTintColor: '#36B6B6',
-        tabBarInactiveTintColor: '#2D3142',
-        tabBarLabelStyle: { fontSize: 14 },
-        tabBarIndicatorStyle: { backgroundColor: '#36B6B6' },
-        tabBarStyle: { backgroundColor: 'transparent' },
-      }}
-      style={{ marginTop: 40}}
-      title='Library'
-    >
-      <TopTab.Screen name="InProgress" component={InProgressPage} />
-      <TopTab.Screen name="Liked" component={LikedPage} />
-      <TopTab.Screen name="Completed" component={CompletedPage} />
-    </TopTab.Navigator>
-  );
-}
-
-function TabStack() {
-  return (
-    <Tab.Navigator
-    screenOptions={({ route }) => ({
-        tabBarActiveTintColor: "#36B6B6",
-        tabBarInactiveTintColor: "#7C7C7C",
-        tabBarLabelStyle: {
-          fontFamily: "Montserrat_400Regular",
-          fontSize: 10,
-        },
-      })}>
-      <Tab.Screen
-        name="Acceuil"
-        component={HomePage}
-        options={{
-          tabBarLabel: 'Home',
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}  />
-        <Tab.Screen
-        name="Biblioteque"
-        component={TopTabs}
-        options={{
-          tabBarLabel: 'Library',
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="library" size={size} color={color} />
-          ),
-        }} />
-      <Tab.Screen
-        name="Profil"
-        component={ProfilePage}
-        options={{
-          tabBarLabel: 'Profile',
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person" size={size} color={color} />
-          ),
-        }} />
-    </Tab.Navigator>
-  );
-}
 
 function App() {
 let [fontsLoaded] = useFonts({
